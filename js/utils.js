fe70d9835dbd5068c7eef8c040f492a17e9d5817
Ivan Cabañas <icabanas1986@hotmail.com>
2025-08-09 18:03:50 -0600

    AjaxSep

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,18 @@
-function validacorreo()
+function validaCedula()
 {
-    
+    var ced = $("#cedula").val();
+    $.ajax({
+        url:"https://www.cedulaprofesional.sep.gob.mx/cedula/buscaCedulaJson.action?json={'maxResult':'100','nombre':'','paterno':'','materno':'','idCedula':'6971771'}",
+        method:"GET",
+        dataType: "json",
+        succes:function(data)
+        {
+            $("#titulo").val(data.titulo);
+        },
+        error: function(xhr,status,error)
+        {
+            console.error("Error:",error);
+            alert("No existe la cedula.");
+        }
+    })
 }
